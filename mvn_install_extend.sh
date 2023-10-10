32477bdf06c51eb609eced3f92ca97d818c7bb41
Yong Qian <138015365+doudoutiantian@users.noreply.github.com>
2023-10-10 11:43:54 +0800

    mvn install 扩展工具

---
+++ mvn_install_extend.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#mvn对应的本地库根目录
+mvn_home='/Users/tanzhenhua/.m2/repository'
+#获取当前maven项目的项目名称
+cur_dir=`pwd|awk -F'/' '{print $NF}'`
+
+#mvn本地库中的项目路径
+snap_dir=`find $mvn_home -name "$cur_dir"`
+echo 'mvn路径:'$snap_dir
+
+#按照更新时间倒排,保留最新更新的2个snapshot
+ls -dt $snap_dir/*/|tail -n +3|awk '{print $NF}'|xargs rm -rf
+
+#执行mvn install命令
+mvn clean install -U -DskipTests
