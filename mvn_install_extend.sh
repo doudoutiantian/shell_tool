20cfdf482a4b812b422d332ac023127479cb5c34
Yong Qian <138015365+doudoutiantian@users.noreply.github.com>
2023-10-11 11:30:25 +0800

    Update mvn_install_extend.sh
    1. 增加执行路径参数传入
    2. 增加执行路径是否是maven项目判定

--- mvn_install_extend.sh
+++ mvn_install_extend.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
-#主要是为了解决mvn本地打包后，mvn本地仓库留有太多的jar，占用太多的本地磁盘
+exec_dir="$1"
+
+if [ -z "$exec_dir" ];then
+exec_dir=`pwd`
+fi
+echo "执行路径:"$exec_dir
+
+
+#判断执行路径是否是mvn项目
+if [ ! -e "$exec_dir/pom.xml" ]; then
+    echo "执行路径($exec_dir)为非maven项目"
+    exit
+fi
 
 #mvn对应的本地库根目录
 mvn_home='/Users/tanzhenhua/.m2/repository'
 #获取当前maven项目的项目名称
-cur_dir=`pwd|awk -F'/' '{print $NF}'`
+cur_dir=`echo $exec_dir|awk -F'/' '{print $NF}'`
 
 #mvn本地库中的项目路径
 snap_dir=`find $mvn_home -name "$cur_dir"`
